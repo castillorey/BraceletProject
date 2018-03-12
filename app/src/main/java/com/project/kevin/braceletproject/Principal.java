5707afe66d9c11231a283824a14b0094b9ffb104
Kevin Castillo <castilloreyeskm@gmail.com>
2018-03-12 12:24:34 -0500

    Avance Interfaz y asignacion de variables

---
+++ app/src/main/java/com/project/kevin/braceletproject/Principal.java
@@ -0,0 +1,74 @@
+package com.project.kevin.braceletproject;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Principal extends AppCompatActivity {
+
+    private EditText cantManillas;
+    private Button botonCalcular;
+    private TextView valorCosto, valorDivisa;
+    private Spinner tipoDivisa, tipoMaterial, tipoDije, tipoMetal;
+    private Resources recursos;
+    private String divisas[], materiales[], dijes[], metales[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        cantManillas = findViewById(R.id.txtCantManillas);
+        botonCalcular = findViewById(R.id.btnCalcular);
+        valorCosto = findViewById(R.id.lblCosto);
+        valorDivisa = findViewById(R.id.lblDivisa);
+        tipoDivisa = findViewById(R.id.cmbTipoDivisa);
+        tipoMaterial = findViewById(R.id.cmbTipoMaterial);
+        tipoDije = findViewById(R.id.cmbTipoDije);
+        tipoMetal = findViewById(R.id.cmbTipoMetal);
+        recursos = this.getResources();
+
+        //Setting Spinners
+        divisas = recursos.getStringArray(R.array.tipoDivisa);
+        ArrayAdapter<String> divisasAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,divisas);
+        tipoDivisa.setAdapter(divisasAdapter);
+
+        materiales = recursos.getStringArray(R.array.tipoMaterial);
+        ArrayAdapter<String> materialAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,materiales);
+        tipoMaterial.setAdapter(materialAdapter);
+
+        dijes = recursos.getStringArray(R.array.tipoDije);
+        ArrayAdapter<String> dijesAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,dijes);
+        tipoDije.setAdapter(dijesAdapter);
+
+        metales = recursos.getStringArray(R.array.tipoMetal);
+        ArrayAdapter<String> metalesAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,metales);
+        tipoMetal.setAdapter(metalesAdapter);
+
+    }
+
+    public boolean validarDivisas(){
+        int opcion = tipoDivisa.getSelectedItemPosition();
+
+        if(cantManillas.getText().toString().isEmpty()){
+            cantManillas.requestFocus();
+            cantManillas.setError(recursos.getString(R.string.error_cantidad_vacio));
+            return false;
+        }
+
+        if(Integer.parseInt(cantManillas.getText().toString()) == 0){
+            cantManillas.requestFocus();
+            cantManillas.setError(recursos.getString(R.string.error_cantidad_cero));
+            return false;
+        }
+
+        return true;
+    }
+}
